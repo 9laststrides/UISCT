e7c9a2c577ff8501ec6a57217276fb8955e97c92
9laststrides <akshay.vishwanath9@gmail.com>
2017-01-24 11:58:37 -0800

    BO

---
+++ Doctor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tcs.construct.BO
+{
+   public class Doctor
+    {
+        int doctorId;
+        string doctorName;
+        string doctorDepartment;
+
+        public string DoctorDepartment
+        {
+            get { return doctorDepartment; }
+            set { doctorDepartment = value; }
+        }
+
+        public string DoctorName
+        {
+            get { return doctorName; }
+            set { doctorName = value; }
+        }
+        public int DoctorId
+        {
+            get { return doctorId; }
+            set { doctorId = value; }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tcs.construct.BO
+{
+    public class User
+    {
+        string userName;
+        string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+    }
+}
